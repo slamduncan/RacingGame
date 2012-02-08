35b5afa6e5c477bbcffa878d6e987cfc245b3556
unknown <Kyle@Kyle-PC.(none)>
2012-02-08 13:39:22 -0700

    Added a Sound.h file where I will fully implement the ability to load and play sounds. It's still not working, but I can't identify the problem right now. It may be something to do with the path of the file I'm using to test it, but I'll keep looking into it. If you try and use it, it shouldn't create any problems, but it also won't do anything yet, so I wouldn't bother. -Kyle

---
+++ CPSC585/CPSC585/src/Sound.h
@@ -0,0 +1,86 @@
+#ifndef SOUND_H
+#define SOUND_H
+
+#include "al.h"
+#include "alc.h"
+#include "alut.h"
+
+#include <stdio.h>
+
+
+#define NUM_BUFFERS 1
+#define NUM_SOURCES 1
+#define NUM_ENVIRONMENTS 1
+
+
+
+void LoadSoundFile()
+{
+	ALfloat listenerPos[]={0.0,0.0,4.0};
+	ALfloat listenerVel[]={0.0,0.0,0.0};
+	ALfloat listenerOri[]={0.0,0.0,1.0, 0.0,1.0,0.0};
+
+	ALfloat source0Pos[]={ -2.0, 0.0, 0.0};
+	ALfloat source0Vel[]={ 0.0, 0.0, 0.0};
+
+	ALuint  buffer[NUM_BUFFERS];
+	ALuint  source[NUM_SOURCES];
+	ALuint  environment[NUM_ENVIRONMENTS];
+
+	ALsizei size,freq;
+	ALenum  format;
+	ALvoid  *data;
+
+
+
+	alListenerfv(AL_POSITION,listenerPos);
+    alListenerfv(AL_VELOCITY,listenerVel);
+    alListenerfv(AL_ORIENTATION,listenerOri);
+
+	alGetError(); // clear any error messages
+
+	// Generate buffers, or else no sound will happen!
+	alGenBuffers(NUM_BUFFERS, buffer);
+
+	if(alGetError() != AL_NO_ERROR) 
+	{
+    	printf("- Error creating buffers !!\n");
+    	exit(1);
+	}
+	else
+	{
+    	printf("init() - No errors yet.");
+	}
+
+	ALboolean b = AL_FALSE;
+
+	alutLoadWAVFile("C:/Users/Kyle/Game/CPSC585/CPSC585/Documentation/Music/Main Menu Music.wav",&format,&data,&size,&freq,&b);
+    alBufferData(buffer[0],format,data,size,freq);
+    alutUnloadWAV(format,data,size,freq);
+
+	alGetError(); /* clear error */
+	alGenSources(NUM_SOURCES, source);
+
+	if(alGetError() != AL_NO_ERROR) 
+	{
+    	printf("- Error creating sources !!\n");
+    	exit(2);
+	}
+	else
+	{
+    	printf("init - no errors after alGenSources\n");
+	}
+
+	alSourcef(source[0], AL_PITCH, 1.0f);
+	alSourcef(source[0], AL_GAIN, 1.0f);
+	alSourcefv(source[0], AL_POSITION, source0Pos);
+	alSourcefv(source[0], AL_VELOCITY, source0Vel);
+	alSourcei(source[0], AL_BUFFER,buffer[0]);
+	alSourcei(source[0], AL_LOOPING, AL_TRUE);
+
+	alSourcePlay(source[0]);
+
+}
+
+
+#endif SOUND_H
