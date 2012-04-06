78151957238e210d293ec4f2f56c5735c3a7a755
Stephen Zylka <sealzylka@gmail.com>
2012-04-06 15:00:22 -0600

    Player vehicle now spawns in last place so as to avoid attacks from AI

--- CPSC585/CPSC585/src/GameEngine.cpp
+++ CPSC585/CPSC585/src/GameEngine.cpp
@@ -918,7 +918,7 @@ m.loading(ren, "Cars");
 	//entManager->createCar("model/ship1.lwo", carMass, carT1);	
 	//entManager->createCar("model/ship1.lwo", carMass, carT2);	
 	
-	for(int i = 1; i < 4; i++){
+	for(int i = 4; i > 1; i--){
 		btTransform carT3 = btTransform(btQuaternion(0, 1, 0, 1), btVector3(0.0f, 3.0f, (float)i*-30.0f));	
 		entManager->createCar("model/ship1.lwo", carMass, carT3);	
 		btTransform carT4 = btTransform(btQuaternion(0, 1, 0, 1), btVector3(30.0f, 3.0f, (float)i*-30.0f));	
