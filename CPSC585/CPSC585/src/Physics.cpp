3d70343c2ecc0a1d93b9c66d8159c184a76b3ec7
Stephen Zylka <sealzylka@gmail.com>
2012-04-06 14:40:23 -0600

    Mines will now explode on the player who dropped them

--- CPSC585/CPSC585/src/Physics.cpp
+++ CPSC585/CPSC585/src/Physics.cpp
@@ -235,14 +235,12 @@ void Physics::step(btScalar &timeStep)
 			btCollisionObject * carMaybe = oa.at(j);
 			//Todo: check this pointer against all car pointers in carList
 			int index = entityManager->getCarIndexViaPointer(carMaybe);
-			if(index != -1 && mine->carId != index)
-			{
-				entityManager->getCar(index)->chassis->applyCentralForce(btVector3(0,15000.0,0));
+			entityManager->getCar(index)->chassis->applyCentralForce(btVector3(0,15000.0,0));
 
-				dynamicsWorld->removeCollisionObject(entityManager->getMine(i)->physicsObject);
-				entityManager->removeMine(entityManager->getMine(i));
-				i--;
-			}
+			dynamicsWorld->removeCollisionObject(entityManager->getMine(i)->physicsObject);
+			entityManager->removeMine(entityManager->getMine(i));
+			i--;
+
 		}
 	}
 
